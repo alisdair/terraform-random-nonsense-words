6966f3d4c3f87325f0ef2846a17a4d37b103ffb3
Alisdair McDiarmid <alisdair@users.noreply.github.com>
2024-02-05 15:10:13 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,6 @@ resource "random_pet" "nonsense" {
   length = var.length
   separator = " "
   keepers = { uuid = uuid() }
-
-  provisioner "local-exec" {
-    command = "echo sleeping && sleep 12 && echo still sleeping && sleep 12 && echo almost done && sleep 12 && echo done"
-  }
-
-  provisioner "local-exec" {
-    command = "ls /"
-  }
 }
 
 output "words" {
